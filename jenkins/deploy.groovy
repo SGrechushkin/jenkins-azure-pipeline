aeb066d35bafa38099a232c3beb0686bc0a84108
SGrechushkin <sgrechushkin@gmail.com>
2025-02-21 09:17:55 +0200

    Credentials1

--- jenkins/deploy.groovy
+++ jenkins/deploy.groovy
@@ -1,9 +1,10 @@
 pipeline {
     agent any
     environment {
-        // Задайте тут змінні середовища для ваших Azure credentials, якщо потрібно
-        AZURE_CLIENT_ID = credentials('a82541c6-ff6d-41f1-b707-223bf98238a0')  // Приклад для string credentials
-        AZURE_CLIENT_SECRET = credentials('a82541c6-ff6d-41f1-b707-223bf98238a0')
+        AZURE_CLIENT_ID = 'your-client-id'
+        AZURE_CLIENT_SECRET = 'your-client-secret'
+        AZURE_TENANT_ID = 'your-tenant-id'
+        AZURE_SUBSCRIPTION_ID = 'your-subscription-id'
     }
     stages {
         stage('Checkout Code') {
@@ -13,24 +14,15 @@ pipeline {
         }
         stage('Terraform Init') {
             steps {
-                script {
-                    // Тут можете використати ці змінні для ініціалізації Terraform
-                    withCredentials([string(credentialsId: 'a82541c6-ff6d-41f1-b707-223bf98238a0', variable: 'AZURE_CLIENT_ID')]) {
-                        sh 'cd terraform && terraform init'
-                    }
-                }
+                sh 'cd terraform && terraform init'
             }
         }
         stage('Terraform Apply') {
             steps {
-                script {
-                    withCredentials([string(credentialsId: 'a82541c6-ff6d-41f1-b707-223bf98238a0', variable: 'AZURE_CLIENT_SECRET')]) {
-                        sh '''
-                        cd terraform
-                        terraform apply -auto-approve
-                        '''
-                    }
-                }
+                sh '''
+                cd terraform
+                terraform apply -auto-approve
+                '''
             }
         }
         stage('Ansible Setup') {
