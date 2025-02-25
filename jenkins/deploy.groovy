2c7e58353623f108dab2aee6d057553acd531024
SGrechushkin <sgrechushkin@gmail.com>
2025-02-25 09:05:46 +0200

    withCredentials_new

--- jenkins/deploy.groovy
+++ jenkins/deploy.groovy
@@ -9,9 +9,19 @@ pipeline {
         stage('Terraform Init & Apply') {
             steps {
                 script {
-                    withCredentials(credentialsId: 'a82541c6-ff6d-41f1-b707-223bf98238a0') {
+                    withCredentials([
+                        string(credentialsId: 'azure-subscription-id', variable: 'AZURE_SUBSCRIPTION_ID'),
+                        string(credentialsId: 'azure-client-id', variable: 'AZURE_CLIENT_ID'),
+                        string(credentialsId: 'azure-client-secret', variable: 'AZURE_CLIENT_SECRET'),
+                        string(credentialsId: 'azure-tenant-id', variable: 'AZURE_TENANT_ID')
+                    ]) {
                         sh '''
                         cd terraform
+                        export TF_VAR_subscription_id=$AZURE_SUBSCRIPTION_ID
+                        export TF_VAR_client_id=$AZURE_CLIENT_ID
+                        export TF_VAR_client_secret=$AZURE_CLIENT_SECRET
+                        export TF_VAR_tenant_id=$AZURE_TENANT_ID
+                        
                         terraform init
                         terraform apply -auto-approve
                         '''
